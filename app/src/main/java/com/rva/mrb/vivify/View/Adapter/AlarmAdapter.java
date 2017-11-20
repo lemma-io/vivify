f4572acf66474ffbd0d68b6b99d7957396f57421
Rodrigo Moran <rigo.moran0@gmail.com>
2017-11-20 17:38:28 -0500

    Migrated to Realm 4.1.1 and Gradle 3.0. Build now ready for Android Studio 3.0

--- app/src/main/java/com/rva/mrb/vivify/View/Adapter/AlarmAdapter.java
+++ app/src/main/java/com/rva/mrb/vivify/View/Adapter/AlarmAdapter.java
@@ -1,12 +1,10 @@
 package com.rva.mrb.vivify.View.Adapter;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -18,15 +16,10 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.rva.mrb.vivify.Model.Data.Alarm;
 import com.rva.mrb.vivify.Model.Service.AlarmScheduler;
-import com.rva.mrb.vivify.Model.Service.RealmService;
 import com.rva.mrb.vivify.R;
-import com.rva.mrb.vivify.View.Detail.DetailActivity;
 import com.rva.mrb.vivify.View.Alarm.AlarmsPresenter;
 
 
-import org.parceler.Parcels;
-
-import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -37,12 +30,12 @@ import butterknife.ButterKnife;
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.realm.RealmBasedRecyclerViewAdapter;
-import io.realm.RealmResults;
-import io.realm.RealmViewHolder;
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmRecyclerViewAdapter;
+
 
 public class AlarmAdapter extends
-        RealmBasedRecyclerViewAdapter<Alarm, AlarmAdapter.ViewHolder> {
+        RealmRecyclerViewAdapter<Alarm, AlarmAdapter.ViewHolder> {
 
     public static final String TAG = AlarmAdapter.class.getSimpleName();
 
@@ -51,88 +44,19 @@ public class AlarmAdapter extends
     public AlarmClickListener alarmClickListener;
     @Inject AlarmsPresenter alarmsPresenter;
 
-    public AlarmAdapter(Context context, RealmResults<Alarm> realmResults,
-                        OnAlarmToggleListener listener, AlarmClickListener clickListener, boolean automaticUpdate,
-                        boolean animateResults) {
-        super(context, realmResults, automaticUpdate, animateResults);
-        this.alarmToggleListener = listener;
-        this.alarmClickListener = clickListener;
-    }
-
-    @Override
-    public ViewHolder onCreateRealmViewHolder(ViewGroup viewGroup, int i) {
-        View v = inflater.inflate(R.layout.alarm_view, viewGroup, false);
-        ViewHolder viewHolder = new ViewHolder(v);
-        return viewHolder;
-    }
-
-    @Override
-    public void onBindRealmViewHolder(
-            final AlarmAdapter.ViewHolder viewHolder, final int position) {
-        dispose(viewHolder);
-        final Alarm alarm = realmResults.get(position);
-        viewHolder.timeTv.setText(alarm.getmWakeTime());
-        viewHolder.nameTv.setText(alarm.getAlarmLabel());
-        viewHolder.isSet.setChecked(alarm.isEnabled());
-        viewHolder.mediaInfoTv.setText((alarm.getArtistName() != null ?
-                alarm.getTrackName() + " by " + alarm.getArtistName() : alarm.getTrackName()));
-        viewHolder.mediaInfoTv.setSelected(true);
-
-        viewHolder.alarmBg.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        Glide.with(viewHolder.itemView.getContext())
-                .load(alarm.getTrackImage())
-                .placeholder(ContextCompat.getDrawable(viewHolder.itemView.getContext(), R.drawable.placeholder4))
-                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                .centerCrop()
-                .into(viewHolder.alarmBg);
-
-        viewHolder.cardView.setOnClickListener(view -> {
-            alarmClickListener.onAlarmClick(position, alarm, viewHolder.alarmBg);
-            Log.d(TAG, "Alarm Name: " + alarm.getArtistName());
-
-//            Log.d(TAG, "Opening Detail activity on id: " + alarm.getId());
-//            Intent intent = new Intent(view.getContext(), DetailActivity.class);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//            intent.putExtra("NewAlarm", false);
-//            intent.putExtra("AlarmArtist", alarm.getArtistName());
-//            intent.putExtra("Alarm", Parcels.wrap(alarm));
-//            ActivityOptionsCompat options = ActivityOptionsCompat.
-//                    makeSceneTransitionAnimation(view, viewHolder.alarmBg, "detail");
-//            view.getContext().startActivity(intent);
-            viewHolder.disposable.dispose();
-        });
-
-        viewHolder.isSet.setOnClickListener(v -> {
-            Log.d(TAG, "Toggle alarm id: " + alarm.getId());
-            AlarmScheduler.enableAlarmById(v.getContext(), alarm.getId());
-
-            alarmToggleListener.onAlarmToggle();
-        });
-
-        viewHolder.toggleLayout.setOnClickListener(v -> {
-            viewHolder.isSet.toggle();
-            Log.d(TAG, "Toggle alarm id: " + alarm.getId());
-            AlarmScheduler.enableAlarmById(v.getContext(), alarm.getId());
-            setAlarmTimer(alarm, viewHolder);
-            alarmToggleListener.onAlarmToggle();
-        });
-
-        if (alarm.isValid()) {
-            setAlarmTimer(alarm, viewHolder);
-            viewHolder.disposable = Flowable.interval(1000L, TimeUnit.MILLISECONDS)
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(along -> {
-                        setAlarmTimer(alarm, viewHolder);
-                    });
-        }
+    public AlarmAdapter(OrderedRealmCollection<Alarm> data,
+                        AlarmClickListener clickListener,
+                        OnAlarmToggleListener listener) {
+        super(data, true);
+        alarmToggleListener = listener;
+        alarmClickListener = clickListener;
     }
 
     public void setAlarmTimer(Alarm alarm, ViewHolder viewHolder) {
         if (alarm.isValid()) {
             if (alarm.isEnabled()) {
-                if (alarm.isSnoozed()){
+                if (alarm.isSnoozed())
                     viewHolder.alarmTimer.setText(getTimeUntil(alarm.getSnoozedAt()));
-                }
                 else {
                     viewHolder.alarmTimer.setText(getTimeUntil(alarm.getTime()));
                 }
@@ -156,7 +80,63 @@ public class AlarmAdapter extends
                         TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis))) +"mins";
     }
 
-    public class ViewHolder extends RealmViewHolder {
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.alarm_view, parent, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        dispose(viewHolder);
+
+        final Alarm alarm = getItem(position);//realmResults.get(position);
+        viewHolder.timeTv.setText(alarm.getmWakeTime());
+        viewHolder.nameTv.setText(alarm.getAlarmLabel());
+        viewHolder.isSet.setChecked(alarm.isEnabled());
+        viewHolder.mediaInfoTv.setText((alarm.getArtistName() != null ?
+                alarm.getTrackName() + " by " + alarm.getArtistName() : alarm.getTrackName()));
+        viewHolder.mediaInfoTv.setSelected(true);
+
+        viewHolder.alarmBg.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        Glide.with(viewHolder.itemView.getContext())
+                .load(alarm.getTrackImage())
+                .placeholder(ContextCompat.getDrawable(viewHolder.itemView.getContext(), R.drawable.placeholder4))
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .centerCrop()
+                .into(viewHolder.alarmBg);
+
+        viewHolder.cardView.setOnClickListener(view -> {
+            alarmClickListener.onAlarmClick(position, alarm, viewHolder.alarmBg);
+
+            viewHolder.disposable.dispose();
+        });
+
+        viewHolder.isSet.setOnClickListener(v -> {
+            AlarmScheduler.enableAlarmById(v.getContext(), alarm.getId());
+
+            alarmToggleListener.onAlarmToggle();
+        });
+
+        viewHolder.toggleLayout.setOnClickListener(v -> {
+            viewHolder.isSet.toggle();
+            AlarmScheduler.enableAlarmById(v.getContext(), alarm.getId());
+            setAlarmTimer(alarm, viewHolder);
+            alarmToggleListener.onAlarmToggle();
+        });
+
+        if (alarm.isValid()) {
+            setAlarmTimer(alarm, viewHolder);
+            viewHolder.disposable = Flowable.interval(1000L, TimeUnit.MILLISECONDS)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(along -> {
+                        setAlarmTimer(alarm, viewHolder);
+                    });
+        }
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         @BindView(R.id.alarm_tv) TextView timeTv;
         @BindView(R.id.card_alarms) CardView cardView;
